f82ef92deb49e68054fbc4c4f50ceb58bd614369
Zobēla, Valdis <valdis.zobela@if.lv>
2016-07-02 12:07:20 +0300

    added static instances for algorithms

--- src/Security.HMAC.Owin/Hmac256SigningAlgorith.cs
+++ src/Security.HMAC.Owin/Hmac256SigningAlgorith.cs
@@ -2,8 +2,10 @@ namespace Security.HMAC
 {
     using System.Security.Cryptography;
 
-    public class Hmac256SigningAlgorith : ISigningAlgorithm
+    public sealed class Hmac256SigningAlgorith : ISigningAlgorithm
     {
+        public static readonly Hmac256SigningAlgorith Instance = new Hmac256SigningAlgorith();
+
         public byte[] Sign(byte[] secret, byte[] content)
         {
             byte[] signatureBytes;
--- src/Security.HMAC.Owin/Sha256HashingAlgorithm.cs
+++ src/Security.HMAC.Owin/Sha256HashingAlgorithm.cs
@@ -3,8 +3,10 @@ namespace Security.HMAC
     using System.IO;
     using System.Security.Cryptography;
 
-    public class Sha256HashingAlgorithm : IHashingAlgorithm
+    public sealed class Sha256HashingAlgorithm : IHashingAlgorithm
     {
+        public static Sha256HashingAlgorithm Instance = new Sha256HashingAlgorithm();
+
         public byte[] ComputeHash(Stream content)
         {
             long pos = content.Position;
