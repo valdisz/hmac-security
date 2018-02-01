5deec1dde3edcb1fd440bc51b5af354f2e6ec9ac
Pavels Sergijenko <pavels.sergijenko@if.lv>
2018-02-01 14:49:14 +0200

    Updated DefaultTolerance to 30. On heavy load server it could be not enough to process security request.

--- src/Core/Constants.cs
+++ src/Core/Constants.cs
@@ -4,7 +4,7 @@ namespace Security.HMAC
 
     public static class Constants
     {
-        public static readonly TimeSpan DefaultTolerance = TimeSpan.FromSeconds(3);
+        public static readonly TimeSpan DefaultTolerance = TimeSpan.FromSeconds(30);
 
         public static readonly DateTimeOffset UnixEpoch = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
     }
